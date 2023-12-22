f5719d3a0cc98b66dddb58682b9c9b5f5faa9e68
bintangtiara <bintang.tiara77@gmail.com>
2023-12-22 14:21:38 +0700

    Add url & dummy

---
+++ app/src/main/java/com/dicoding/beasify/dummy/DataDummy.kt
@@ -0,0 +1,15 @@
+package com.dicoding.beasify.dummy
+
+import com.dicoding.beasify.data.Api
+
+object DataDummy {
+    val dummyApi = Api(
+        nama = "John Doe",
+        umur = 25,
+        universitas = "Cambridge University",
+        jurusan = "Computer Science",
+        semester = 3,
+        IPK = 3,
+        TOEFL = 'A'
+    )
+}
